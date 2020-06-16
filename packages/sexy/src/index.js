a14388ae383b6f44a879d1f73753c72a91b18939
Kirill <lastonoga@gmail.com>
2020-06-16 13:23:09 +0400

    Disable tree-shaking

--- packages/sexy/src/index.js
+++ packages/sexy/src/index.js
@@ -1,49 +1,53 @@
-export {
-	render,
-	hydrate,
-	refresh
-} from 'sexy-framework/render';
+// export {
+// 	render,
+// 	hydrate,
+// 	refresh
+// } from 'sexy-framework/render';
 
-import { observable, computed, subscribe, watch } from 'sexy-framework/observable';
+require('sexy-framework/observable');
+require('sexy-framework/transition');
+require('sexy-framework/render');
 
-import {
-	attrs as _makeAttrs$,
-	events as _makeEvents$,
-	slot as _slot$,
-	map as _each$,
-	statement as _statement$,
-	directive as _directive$,
-	getNode,
-	getProp as _getProp$,
-	setRef as _setRef$,
-	setKey as _setKey$,
-	emit as _emit$,
-	call as _call$,
-	createComponent as _createComponent$,
-	createHooks as _createHooks$,
-	parseContext,
-	loadComponent,
-} from 'sexy-framework/render';
+// import { observable, computed, subscribe, watch } from 'sexy-framework/observable';
 
-import {
-	transition as _transition$,
-} from 'sexy-framework/transition'
+// import {
+// 	attrs as _makeAttrs$,
+// 	events as _makeEvents$,
+// 	slot as _slot$,
+// 	map as _each$,
+// 	statement as _statement$,
+// 	directive as _directive$,
+// 	getNode,
+// 	getProp as _getProp$,
+// 	setRef as _setRef$,
+// 	setKey as _setKey$,
+// 	emit as _emit$,
+// 	call as _call$,
+// 	createComponent as _createComponent$,
+// 	createHooks as _createHooks$,
+// 	parseContext,
+// 	loadComponent,
+// } from 'sexy-framework/render';
+
+// import {
+// 	transition as _transition$,
+// } from 'sexy-framework/transition'
 
 
-_makeAttrs$.bind(null);
-_makeEvents$.bind(null);
-_slot$.bind(null);
-_each$.bind(null);
-_statement$.bind(null);
-_directive$.bind(null);
-_getProp$.bind(null);
-_setRef$.bind(null);
-_setKey$.bind(null);
-_emit$.bind(null);
-_call$.bind(null);
-_createComponent$.bind(null);
-_createHooks$.bind(null);
-getNode.bind(null);
-parseContext.bind(null);
-loadComponent.bind(null);
-_transition$.bind(null);
+// _makeAttrs$.bind(null);
+// _makeEvents$.bind(null);
+// _slot$.bind(null);
+// _each$.bind(null);
+// _statement$.bind(null);
+// _directive$.bind(null);
+// _getProp$.bind(null);
+// _setRef$.bind(null);
+// _setKey$.bind(null);
+// _emit$.bind(null);
+// _call$.bind(null);
+// _createComponent$.bind(null);
+// _createHooks$.bind(null);
+// getNode.bind(null);
+// parseContext.bind(null);
+// loadComponent.bind(null);
+// _transition$.bind(null);
