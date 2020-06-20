d25d4b4d4841c430d9ed99034ec182490864cd1c
Kirill <lastonoga@gmail.com>
2020-06-20 17:53:53 +0300

    Refactor client

--- packages/sexy-server/src/internal/client.js
+++ packages/sexy-server/src/internal/client.js
@@ -1,5 +1,22 @@
-import { router } from './router';
+import APP_ROUTES from 'sexy-routes';
+import navigo from 'navigo';
+import { hydrate } from 'sexy-framework/render';
+
+var router = new navigo();
 
 const root = document.getElementById('_layout');
 
-router.start(root);
+for(let route in APP_ROUTES) {
+	router.on(route, function (params, query) {
+		// console.time('hydrate');
+		hydrate(APP_ROUTES[route](), root)
+		// console.timeEnd('hydrate');
+	})
+}
+
+router.on('*', function (query) {
+	hydrate(APP_ROUTES['/error-404'](), root);
+});
+
+router.resolve();
+
