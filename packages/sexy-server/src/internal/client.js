5ecd42c35a80c24122669b44579831a8878d2cde
Kirill <lastonoga@gmail.com>
2020-06-21 06:05:57 +0300

    Add not found and conditional render/hydration

--- packages/sexy-server/src/internal/client.js
+++ packages/sexy-server/src/internal/client.js
@@ -1,22 +1,35 @@
 import APP_ROUTES from 'sexy-routes';
 import navigo from 'navigo';
-import { hydrate } from 'sexy-framework/render';
+import { render, hydrate } from 'sexy-framework/render';
 
-var router = new navigo();
+var router = new navigo(location.protocol + "//" + location.host, false);
 
 const root = document.getElementById('_layout');
 
-for(let route in APP_ROUTES) {
-	router.on(route, function (params, query) {
-		// console.time('hydrate');
-		hydrate(APP_ROUTES[route](), root)
-		// console.timeEnd('hydrate');
-	})
+let isSSR = true;
+
+function make()
+{
+	let fn = render;
+	
+	if(isSSR) {
+		fn = hydrate;
+	}
+
+	isSSR = false;
+
+	return fn.apply(this, arguments);
 }
 
-router.on('*', function (query) {
-	hydrate(APP_ROUTES['/error-404'](), root);
+router.notFound(function () {
+	make(APP_ROUTES['/error-404'](), root)
 });
+
+for(let route in APP_ROUTES) {
+	router.on(route, function (params, query) {
+		make(APP_ROUTES[route](), root)
+	});
+}
 
 router.resolve();
 
