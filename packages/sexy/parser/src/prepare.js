fc9291e26621f5528f6677e3cf208dd2aafa05c3
Kirill <lastonoga@gmail.com>
2020-06-25 17:53:13 +0400

    Remove group

--- packages/sexy/parser/src/prepare.js
+++ packages/sexy/parser/src/prepare.js
@@ -7,16 +7,18 @@ export function prepare(html)
 {
 	html = html
 		// if
-		.replace(/([^\\])\@if\((.*)\)/g, '<expr type="statement"><expr type="program" value="$1">')
-		.replace(/([^\\])\@elseif\((.*)\)/g, '</expr><expr type="program" value="$1">')
-		.replace(/([^\\])\@else/g, '</expr><expr type="program" value="true">')
-		.replace(/([^\\])\@endif/g, '</expr></expr>')
+		.replace(/[^\\]\@if\((.*)\)/g, '<expr type="statement"><expr type="program" value="$1">')
+		.replace(/[^\\]\@elseif\((.*)\)/g, '</expr><expr type="program" value="$1">')
+		.replace(/[^\\]\@else/g, '</expr><expr type="program" value="true">')
+		.replace(/[^\\]\@endif/g, '</expr></expr>')
 		// each
-		.replace(/([^\\])\@each\((.*)\)/g, '<expr type="each" value="$1">')
-		.replace(/([^\\])\@endeach/g, '</expr>')
+		.replace(/[^\\]\@each\((.*)\)/g, '<expr type="each" value="$1">')
+		.replace(/[^\\]\@endeach/g, '</expr>')
 		// Sexy slot for server layouts
 		.replace(/\<sexy(\s*)\/\>/g, '<slot name="sexy"></slot>')
 		.replace(/\\@/g, '@');
+
+		console.log(html);
 
 	return prepareHTML(html);
 }
